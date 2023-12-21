3a42090e0c9f7b057a76d9ca69847c38652d8fa3
Astro <astro@spaceboyz.net>
2023-12-21 02:00:26 +0100

    relay: bump worker channel capacity

--- src/relay.rs
+++ src/relay.rs
@@ -104,7 +104,7 @@ struct Job {
 }
 
 fn spawn_worker(client: Arc<reqwest::Client>) -> Sender<Job> {
-    let (tx, mut rx) = channel(8);
+    let (tx, mut rx) = channel(128);
 
     tokio::spawn(async move {
         let mut errors = 0u32;
